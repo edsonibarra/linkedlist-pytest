ae38819f4da1a9b42216305113f267f24cc332bd
edsonibarra <edsonibarra97@gmail.com>
2022-11-19 12:05:54 -0600

    fundamental testing

--- test_linked_list.py
+++ test_linked_list.py
@@ -1,3 +1,4 @@
+import pytest
 from node import Node
 from linkedlist import LinkedList
 
@@ -5,3 +6,27 @@ from linkedlist import LinkedList
 def test_creation_linked_list():
     linkedlist = LinkedList()
     assert isinstance(linkedlist, LinkedList)
+
+def test_attributes_linked_list():
+    linkedlist = LinkedList()
+    assert linkedlist.head is None
+
+def test_attributes_node():
+    node = Node(1)
+    assert node.next is None
+    linkedlist = LinkedList()
+    linkedlist.head = node
+    assert node.data == 1
+    assert linkedlist.head.data == 1
+
+def test_append_linked_list_method():
+    linkedlist = LinkedList()
+
+    linkedlist.append(1)
+    linkedlist.append(2)
+    linkedlist.append(3)
+    linkedlist.append(4)
+
+    assert linkedlist.head.data == 1
+    assert linkedlist.head.next.data == 2
+    assert len(linkedlist) == 4
